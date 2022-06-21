881962f00b40d60e4d3646a923bb277e313dabf3
LuccaSaraiva <luccasarfer@gmail.com>
2022-06-21 15:54:07 -0300

    Exercício de variáveis concluído

---
+++ Fundamentos/4. JavaScript/variaveis.js
@@ -0,0 +1,13 @@
+const myName = "Lucca";
+const birthCity = "São Paulo";
+var birthYear = 2002;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+var birthYear = 2030;
+
+console.log(birthYear);
+
+const birthCity = "Pirajuí";
