84c0312a7e11797c0544101d39ce5d6159c93b98
LuccaSaraiva <luccasarfer@gmail.com>
2022-06-23 18:59:41 -0300

    2/4 - Agora a prática

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/agoraapratica.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/agoraapratica.js
@@ -1,12 +1,21 @@
 // Exercício 1
-let fatorial = 2;
+// let fatorial = 2;
 
-for (let i = 1; i <= 10; i++) {
-  fatorial = fatorial * i;
-    if (fatorial = 3628800) {
-        break
-    }
+// for (let i = 1; i <= 10; i++) {
+//  fatorial = fatorial * i;
+//    if (fatorial = 3628800) {
+//        break
+//    }
+// }
+//
+// console.log(fatorial);
+
+// Exercício 2
+let word = "tô fritando o côco";
+let newWord = "";
+
+for (let i = word.length - 1; i >= 0; i--) {
+  newWord += word[i];
 }
 
-console.log(fatorial);
-
+console.log(newWord);
