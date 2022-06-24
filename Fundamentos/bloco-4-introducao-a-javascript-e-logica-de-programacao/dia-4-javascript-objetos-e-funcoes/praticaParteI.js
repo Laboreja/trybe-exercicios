f6ca3dacaebb42942c63a5a94626cefba6c11a09
LuccaSaraiva <luccasarfer@gmail.com>
2022-06-24 19:04:02 -0300

    6/8 - Parte I - Objetos e For/In

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/praticaParteI.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/praticaParteI.js
@@ -1,10 +1,10 @@
 // Exercício 1 & Exercício 2
-let info1 = {
-  personagem: "Margarida",
-  origem: "Pato Donald",
-  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-  recorrente: "Sim",
-};
+// let info1 = {
+//   personagem: "Margarida",
+//   origem: "Pato Donald",
+//   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+//   recorrente: "Sim",
+// };
 
 // console.log("Bem-vinda, " + info.personagem);
 
@@ -22,19 +22,35 @@ let info1 = {
 // }
 
 // Exercício 5
-let info2 = {
-  personagem: "Tio Patinhas",
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: "O último MacPatinhas",
-  recorrente: "Sim",
+// let info2 = {
+//   personagem: "Tio Patinhas",
+//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota: "O último MacPatinhas",
+//   recorrente: "Sim",
+// };
+
+// console.log(info1.personagem + " e " + info2.personagem);
+// console.log(info1.origem + " e " + info2.origem);
+// console.log(info1.nota + " e " + info2.nota);
+
+// if (info1.recorrente === info2.recorrente) {
+//   console.log("Ambos recorrentes");
+// } else {
+//   console.log(">:(");
+// }
+
+// Exercício 6
+let leitor = {
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+  ],
 };
 
-console.log(info1.personagem + " e " + info2.personagem);
-console.log(info1.origem + " e " + info2.origem);
-console.log(info1.nota + " e " + info2.nota);
-
-if (info1.recorrente === info2.recorrente) {
-  console.log("Ambos recorrentes");
-} else {
-  console.log(">:(");
-}
+console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + "'" + leitor["livrosFavoritos"][0].titulo + "'.");
