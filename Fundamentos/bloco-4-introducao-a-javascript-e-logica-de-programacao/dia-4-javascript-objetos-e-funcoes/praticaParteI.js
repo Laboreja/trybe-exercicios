6aa177889c2d7150ff1bf4b98268ba63a45d39cb
LuccaSaraiva <luccasarfer@gmail.com>
2022-06-24 18:49:03 -0300

    2/8 - Parte I - Objetos e For/In

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/praticaParteI.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/praticaParteI.js
@@ -1,8 +1,12 @@
-// Exercício 1
+// Exercício 1 & Exercício 2
 let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim"
 };
 
 console.log("Bem-vinda, " + info.personagem);
+
+// Exercício 2
+console.log(info);
