3dbd04f1c7d8e2e720809fb6838e6f58b5b36000
LuccaSaraiva <luccasarfer@gmail.com>
2022-06-24 19:15:05 -0300

    8/8 - Parte I - Objetos e For/In

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/praticaParteI.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/praticaParteI.js
@@ -41,16 +41,16 @@
 
 // Exercício 6
 let leitor = {
-  nome: "Julia",
-  sobrenome: "Pessoa",
-  idade: 21,
-  livrosFavoritos: [
-    {
-      titulo: "O Pior Dia de Todos",
-      autor: "Daniela Kopsch",
-      editora: "Tordesilhas",
-    },
-  ],
+    nome: "Julia",
+    sobrenome: "Pessoa",
+    idade: 21,
+    livrosFavoritos: [
+        {
+            titulo: "O Pior Dia de Todos",
+            autor: "Daniela Kopsch",
+            editora: "Tordesilhas",
+        },
+    ],
 };
 
 console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + "'" + leitor["livrosFavoritos"][0].titulo + "'.");
@@ -62,4 +62,7 @@ leitor["livrosFavoritos"].push({
     editora: 'Rocco',
 });
 
-console.log(leitor["livrosFavoritos"])
+console.log(leitor["livrosFavoritos"]);
+
+// Exercício 8
+console.log(leitor.nome + " tem " + leitor["livrosFavoritos"].length + " livros favoritos.");
