2d69a5681ac0ad71eb1fa7fcdd4dc764df46a411
LuccaSaraiva <luccasarfer@gmail.com>
2022-06-24 18:57:12 -0300

    5/8 - Parte I - Objetos e For/In

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/praticaParteI.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/praticaParteI.js
@@ -1,5 +1,5 @@
 // Exercício 1 & Exercício 2
-let info = {
+let info1 = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
@@ -17,6 +17,24 @@ let info = {
 // }
 
 // Exercício 4
-for (i in info) {
-  console.log(info[i]);
+// for (i in info) {
+//   console.log(info[i]);
+// }
+
+// Exercício 5
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+};
+
+console.log(info1.personagem + " e " + info2.personagem);
+console.log(info1.origem + " e " + info2.origem);
+console.log(info1.nota + " e " + info2.nota);
+
+if (info1.recorrente === info2.recorrente) {
+  console.log("Ambos recorrentes");
+} else {
+  console.log(">:(");
 }
