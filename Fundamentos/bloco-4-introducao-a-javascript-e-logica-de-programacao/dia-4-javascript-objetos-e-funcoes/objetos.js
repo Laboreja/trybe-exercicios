015838be12824e487ca14f48dd858d77ad909d2a
LuccaSaraiva <luccasarfer@gmail.com>
2022-06-24 15:52:25 -0300

    3/4 + 4/4 - 'Objetos'

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos.js
@@ -0,0 +1,20 @@
+// Exercício 1 + Exercício 3
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3
+    },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+}
+
+// Exercício 2
+console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade.");
+
+// Exercício 4
+console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor do mundo por em " + player.bestInTheWorld)
+
+// Exercício 5
+console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata.");
