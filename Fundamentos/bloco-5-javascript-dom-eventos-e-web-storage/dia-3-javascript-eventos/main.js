e1900979fc00dea2c3f0f8a617b93df1396d6717
LuccaSaraiva <luccasarfer@gmail.com>
2022-06-30 18:35:53 -0300

    4/5 - Exercícios - Parte II - Conheça o addEventListener

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
@@ -33,6 +33,12 @@ document.getElementsByClassName("tech");
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+// R: Infelizmente não fiz o meu ainda, então será para outro canto
+let spotrybefyzinha = document.getElementById("my-spotrybefy");
+spotrybefyzinha.addEventListener("dblclick", linkado);
+function linkado(event) {
+  window.open("https://youtu.be/-MaCJZIBKGs");
+}
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere a cor do mesmo;
 
