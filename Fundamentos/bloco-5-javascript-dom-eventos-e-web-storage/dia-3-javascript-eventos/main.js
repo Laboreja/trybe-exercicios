09aa277e1113205f0691bcfd1726ae4340aa0a2a
LuccaSaraiva <luccasarfer@gmail.com>
2022-06-30 18:27:31 -0300

    VAMO QUE VAMO 3/5 - Exercícios - Parte II - Conheça o addEventListener

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
@@ -6,28 +6,35 @@ const myWebpage = document.getElementById("my-spotrybefy");
 
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
-// 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre? . R: Por causa da classe
+// 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+// R: Por causa da classe
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 document.getElementById("first-li").addEventListener("click", clicaTech);
 document.getElementById("second-li").addEventListener("click", clicaTech);
 document.getElementById("third-li").addEventListener("click", clicaTech);
 function clicaTech() {
-    document.getElementById("first-li").className = 'tech';
-    document.getElementById("second-li").className = 'tech';
-    document.getElementById("third-li").className = 'tech';
+  document.getElementById("first-li").className = "tech";
+  document.getElementById("second-li").className = "tech";
+  document.getElementById("third-li").className = "tech";
 }
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+// R: Só eu não ativar a minha função clicaTech
 
-// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
+// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech';
+let firstLis = document.getElementById("first-li");
+let caixinhaDeTexto = document.getElementById("input");
+caixinhaDeTexto.addEventListener("input", inputa);
+function inputa(event) {
+  firstLis.innerText = input.value;
+}
 
-// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-// redirecione para alguma página;
+document.getElementsByClassName("tech");
+
+// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 
-// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-// a cor do mesmo;
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere a cor do mesmo;
 
 // Segue abaixo um exemplo do uso de event.target:
 
